c55b7927847c6e807dc2e6a27e03b27fea616205
Drew Kestell <drew.kestell@gmail.com>
2017-10-31 21:26:12 -0500

    Initial commit

---
+++ AudioTranscriber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Google.Cloud.Speech.V1;
+
+namespace AudioTranscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = "gs://<bucket-name>/<file-name>";
+
+            var speech = SpeechClient.Create();
+            var longOperation = speech.LongRunningRecognize(new RecognitionConfig()
+            {
+                Encoding = RecognitionConfig.Types.AudioEncoding.Flac,
+                SampleRateHertz = 16000,
+                LanguageCode = "en",
+            }, RecognitionAudio.FromStorageUri(uri));
+
+            longOperation = longOperation.PollUntilCompleted();
+
+            var response = longOperation.Result;
+
+            foreach (var result in response.Results)
+            {
+                foreach (var alternative in result.Alternatives)
+                {
+                    Console.WriteLine(alternative.Transcript);
+                }
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
